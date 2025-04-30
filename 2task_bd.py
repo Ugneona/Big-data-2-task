c99b1f29a5ff4acbc9732849e21f2003ac1ac526
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2025-04-30 23:51:07 +0300

    Update 2task_bd.py

--- 2task_bd.py
+++ 2task_bd.py
@@ -113,5 +113,6 @@ def index():
     return Response(html, mimetype='text/html')  # Return the full HTML response with all plots and reports
 
 if __name__ == "__main__":
-    app.run()  # Run the Flask application
+       app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+
 
