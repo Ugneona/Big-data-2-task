1f3bb3e9d5309da1584384020a081abf991c8994
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2025-05-01 00:02:59 +0300

    Update 2task_bd.py

--- 2task_bd.py
+++ 2task_bd.py
@@ -113,6 +113,6 @@ def index():
     return Response(html, mimetype='text/html')  # Return the full HTML response with all plots and reports
 
 if __name__ == "__main__":
-       app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+       app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
 
